2bede6e3f6cedc3e7517a31bc2ac95875cef312f
valery <vbrasseur@gmail.com>
2017-11-21 16:20:42 +0100

    add /competitions for testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
+var sqlite3 = require('sqlite3').verbose();
+
 var express = require('express');
 var router = express.Router();
+
+var db = new sqlite3.Database('tables.sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/competitions', function(req, res){
+        res.header('Charset','utf8');
+
+    db.all("SELECT * FROM competitions", function(err, rows){
+				if(err)
+          console.log("Error Selecting : %s ",err );
+
+        res.render('competitions',{title:"Test Table",data:rows});
+      });
+
+        //res.json({ "competitions" : row });
 });
 
 module.exports = router;
