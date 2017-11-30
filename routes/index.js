d75eab075db7dd6c5aa71f65a3ddaddfd96ee740
valery <vbrasseur@gmail.com>
2017-11-30 17:32:20 +0100

    read for MyTarget DB

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var sqlite3 = require('sqlite3').verbose();
 var express = require('express');
 var router = express.Router();
 
-var db = new sqlite3.Database('tables.sqlite3');
+var db = new sqlite3.Database('MyTargets.sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,14 +13,60 @@ router.get('/', function(req, res, next) {
 router.get('/competitions', function(req, res){
         res.header('Charset','utf8');
 
-    db.all("SELECT * FROM competitions", function(err, rows){
+    db.all("SELECT * FROM Training", function(err, rows){
 				if(err)
           console.log("Error Selecting : %s ",err );
 
-        res.render('competitions',{title:"Test Table",data:rows});
+        res.render('competitions',{title:"Competitions",data:rows});
       });
 
         //res.json({ "competitions" : row });
 });
+
+function findTraining(req, res, next) {
+    var competitionId = req.params.id;
+    var dbRequest = 'SELECT * FROM Training WHERE _id = \'' + competitionId + '\'';
+
+    //console.log(dbRequest);
+    db.all(dbRequest, function(error, rows) {
+        if(rows.length !== 0) {
+            req.training = rows;
+            return next();
+        }
+
+        res.render('incorrect_training'); /* Render the error page. */            
+    });
+}
+
+function findRounds(req, res, next) {
+	  var competitionId = req.params.id;
+    dbRequest = 'SELECT * FROM Round WHERE training = \'' + competitionId + '\'';
+    //console.log(dbRequest);
+        db.all(dbRequest, function(error, rows) {
+            /* Add selected data to previous saved data. */
+            req.rounds = rows;
+            next();
+        
+    });
+}
+
+function renderTraining(req, res) {
+        res.header('Charset','utf8');
+
+    //console.log(renderTraining);
+    console.log(req.training);
+    console.log(req.rounds);
+    res.render('detail', {
+			  title:"detail",
+        training: req.training,
+        rounds: req.rounds
+    });
+}
+
+router.get('/details/:id', findTraining, findRounds,  renderTraining);
+
+router.get('/about', function(req, res) {
+        res.render('about.ejs');
+    });
 
 module.exports = router;
